7fd3c6a977ac1071a9edfc090639428b7c2b08a6
dindafildzahasya <dindafilzahasya@gmail.com>
2026-09-26 00:40:27 +0700

    Create Film.php

---
+++ php/Film.php
@@ -0,0 +1,67 @@
+<?php
+
+// Membuat class Film
+class Film{
+    // Atribut private untuk ID film
+    private $id;
+    // Atribut private untuk judul film
+    private $judul;
+    // Atribut private untuk genre film
+    private $genre;
+    // Atribut private untuk durasi film
+    private $durasi;
+
+    // Constructor Film
+    public function __construct($id, $judul, $genre, $durasi){
+        // Menyimpan ID film
+        $this->id = $id;
+        // Menyimpan judul film
+        $this->judul = $judul;
+        // Menyimpan genre film
+        $this->genre = $genre;
+        // Menyimpan durasi film
+        $this->durasi = $durasi;
+    }
+
+    // Getter ID film
+    public function getId(){
+        return $this->id;
+    }
+    // Setter ID film
+    public function setId($id){
+        $this->id = $id;
+    }
+
+    // Getter dan Setter Judul
+    public function getJudul(){
+        return $this->judul;
+    }
+    public function setJudul($judul){
+        $this->judul = $judul;
+    }
+
+    // Getter dan Setter Genre
+    public function getGenre(){
+        return $this->genre;
+    }
+    public function setGenre($genre){
+        $this->genre = $genre;
+    }
+
+    // Getter dan Setter Durasi
+    public function getDurasi(){
+        return $this->durasi;
+    }
+    public function setDurasi($durasi){
+        $this->durasi = $durasi;
+    }
+
+    // Method menampilkan informasi film
+    public function tampilkanInfo(){
+        echo "ID Film : ".$this->id."<br>";
+        echo "Judul : ".$this->judul."<br>";
+        echo "Genre : ".$this->genre."<br>";
+        echo "Durasi : ".$this->durasi." menit<br>";
+    }
+}
+?>
